4070ab9dcf8fb6a99185fd6ecd6652487b74c8ce
Alex Kerr <kirbyman62@gmail.com>
2015-08-24 11:12:33 +0300

    Made the algorithm generic to use any order

--- powell.pl
+++ powell.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
-use Data::Dumper;
 use strict;
+
+# The order to use - this decides the size of each prefix:
+our $ORDER = 2;
 
 # Read the file:
 open my $file, '<', $ARGV[0] or die "Cannot open '$ARGV[0]': $!\n";
@@ -13,36 +15,40 @@ for my $excuse(@excuses)
 	# Split by the word:
 	my @words = split /\s/, $excuse;
 
-	# Loop through all words, creating key/pair values of every every two
-	# words => the third word. For example, 'hello how are you' becomes:
+	# Loop through all words, creating key/pair values of every every n words =>
+	# the (n + 1)th word (where n = $ORDER). For example, 'hello how are you'
+	# (n = 2) becomes:
 	# 	'hello how' => [ 'are' ]
 	#	'how are' => [ 'you' ]
-	my ($i, $j, $k) = (0, 1, 2);
-	while($k < @words)
+	for(my $i = 0; $i < (@words - $ORDER); $i++)
 	{
-		push @{$markov{"$words[${i}++] $words[${j}++]"}}, $words[${k}++];
+		my @selection = @words[$i..($i + $ORDER)];
+		my $a = pop @selection;
+		push @{$markov{join " ", @selection}}, $a;
 	}
 }
-
-# print Dumper(\%markov);
 
 # Start at a capital letter:
 my @starts = grep { $_ =~ /^[A-Z]/ } keys %markov;
 
 # Traverse the chain to generate the excuse:
 my @selected = split /\s/, $starts[rand @starts];
-my $out = $selected[0] . " ";
+my $out = $selected[0] unless(@selected eq 1);
 
 do
 {
-	$out .= $selected[(@selected - 1)] . " ";
+	if(defined $selected[1]) { $out .= " " . $selected[1] } else
+		{ $out .= " " . $selected[0] };
 	push @selected,
 		$markov{join " ", @selected}->[rand @{$markov{join " ", @selected}}];
 	shift @selected;
 }
 while(defined $markov{join " ", @selected});
 
-# Add some punctuation if needed:
+# Add the last word and some punctuation if needed:
+if(@selected > 1) { $out .= " $_" for(@selected[1..(@selected - 1)]); } else
+	{ $out .= " " . $selected[0]; }
+#$out .= " " . $selected[-1];
 $out .= "." unless($out =~ /[.!?]$/);
 
-print $out;
+print $out, "\n";

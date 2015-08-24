9231d9f1c54935662eba31b0a5c0e604625f1218
Alex Kerr <kirbyman62@gmail.com>
2015-08-24 12:47:12 +0300

    Added command line arguments and removed reference to Jordan

--- powell.pl
+++ powell.pl
@@ -1,54 +1,90 @@
 #!/usr/bin/perl -w
+use Getopt::Long;
 use strict;
 
-# The order to use - this decides the size of each prefix:
-our $ORDER = 2;
+# Subroutine prototypes:
+sub help; # Prints help information and exits.
+sub main; # The main body of the program.
 
-# Read the file:
-open my $file, '<', $ARGV[0] or die "Cannot open '$ARGV[0]': $!\n";
-chomp(my @excuses = <$file>);
+# Process command line options:
+my $help = ""; # Print help (True/false).
+my $capital = ""; # Start the generated text with a capital letter.
+my $order = 2; # The order to use - this decides the size of each prefix.
+my $file = ""; # The file to process.
+GetOptions(
+	"help" => \$help,
+	"capital" => \$capital,
+	"order=i" => \$order,
+);
 
-# Construct the Markov chain
-my %markov;
-for my $excuse(@excuses)
+if($help) { help } else { main }
+
+sub main
 {
-	# Split by the word:
-	my @words = split /\s/, $excuse;
-
-	# Loop through all words, creating key/pair values of every every n words =>
-	# the (n + 1)th word (where n = $ORDER). For example, 'hello how are you'
-	# (n = 2) becomes:
-	# 	'hello how' => [ 'are' ]
-	#	'how are' => [ 'you' ]
-	for(my $i = 0; $i < (@words - $ORDER); $i++)
+	# Read the file(s):
+	my @samples;
+	for my $filename(@ARGV)
 	{
-		my @selection = @words[$i..($i + $ORDER)];
-		my $a = pop @selection;
-		push @{$markov{join " ", @selection}}, $a;
+		open my $file, '<', $filename or die "Cannot open '$filename': $!\n";
+		chomp(my @sample = <$file>);
+		push @samples, @sample;
 	}
+
+	# Construct the Markov chain
+	my %markov;
+	for my $sample(@samples)
+	{
+		# Split by the word:
+		my @words = split /\s/, $sample;
+
+		# Loop through all words, creating key/pair values of every every n
+		# words => the (n + 1)th word (where n = $order). For example, 'hello
+		# how are you' (n = 2) becomes:
+		# 	'hello how' => [ 'are' ]
+		#	'how are' => [ 'you' ]
+		for(my $i = 0; $i < (@words - $order); $i++)
+		{
+			my @selection = @words[$i..($i + $order)];
+			my $a = pop @selection;
+			push @{$markov{join " ", @selection}}, $a;
+		}
+	}
+
+	# Start at a capital letter if enabled:
+	my @starts = ($capital) ? grep { $_ =~ /^[A-Z]/ } keys %markov :
+		keys %markov;
+
+	# Traverse the chain to generate the text:
+	my @selected = split /\s/, $starts[rand @starts];
+	my $out = $selected[0] unless(@selected eq 1);
+
+	do
+	{
+		if(defined $selected[1]) { $out .= " " . $selected[1] } else
+			{ $out .= " " . $selected[0] };
+		push @selected,
+			$markov{join " ", @selected}->[rand @{$markov{join " ", @selected}}];
+		shift @selected;
+	}
+	while(defined $markov{join " ", @selected});
+
+	# Add the last word and some punctuation if needed:
+	if(@selected > 1) { $out .= " $_" for(@selected[1..(@selected - 1)]); } else
+		{ $out .= " " . $selected[0]; }
+	$out .= "." unless($out =~ /[.!?]$/);
+
+	print $out, "\n";
 }
 
-# Start at a capital letter:
-my @starts = grep { $_ =~ /^[A-Z]/ } keys %markov;
-
-# Traverse the chain to generate the excuse:
-my @selected = split /\s/, $starts[rand @starts];
-my $out = $selected[0] unless(@selected eq 1);
-
-do
+# Prints help text and exits:
+sub help
 {
-	if(defined $selected[1]) { $out .= " " . $selected[1] } else
-		{ $out .= " " . $selected[0] };
-	push @selected,
-		$markov{join " ", @selected}->[rand @{$markov{join " ", @selected}}];
-	shift @selected;
+	print "powell: usage: powell [OPTIONS] [FILES]\n\n";
+	print "powell takes any number of files, reads them line by line and uses\n";
+	print " Markov chains to randomly construct a new string.\n\n";
+	print "Options are as follows:\n";
+	print "\t-h or --help:      Displays this help text and quits\n";
+	print "\t-o n or --order=n: Sets the order to use, where n is a positive\n";
+	print "\t non-zero integer. The default order is 2.\n";
+	print "\t-c or --capital:   Start the string with a capital letter.\n";
 }
-while(defined $markov{join " ", @selected});
-
-# Add the last word and some punctuation if needed:
-if(@selected > 1) { $out .= " $_" for(@selected[1..(@selected - 1)]); } else
-	{ $out .= " " . $selected[0]; }
-#$out .= " " . $selected[-1];
-$out .= "." unless($out =~ /[.!?]$/);
-
-print $out, "\n";
